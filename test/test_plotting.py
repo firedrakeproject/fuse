0a89f3ffc9a0b2d898ccfcbf9b1cab3719fc6b2a
India Marsden <imm1117@ic.ac.uk>
2025-04-10 09:22:08 +0100

    add python plotting to test

--- test/test_plotting.py
+++ test/test_plotting.py
@@ -3,16 +3,24 @@ from test_3d_examples_docs import construct_tet_ned, construct_tet_rt, construct
 
 
 def test_2d_elements():
+    """ Tests both tikz generation and python plotting run without error
+
+    Run locally with -rP to view the tikz code."""
     elems = [construct_cg3, construct_nd, construct_rt, construct_hermite]
 
     for e in elems:
         triple = e()
         print(triple.to_tikz())
+        triple.plot()
 
 
 def test_3d_elements():
+    """ Tests both tikz generation and python plotting run without error
+
+    Run locally with -rP to view the tikz code."""
     elems = [construct_tet_ned, construct_tet_rt, construct_tet_cg3]
 
     for e in elems:
         triple = e()
         print(triple.to_tikz())
+        triple.plot()
