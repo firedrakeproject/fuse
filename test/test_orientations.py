fe85a47b047343dcaf7df45dd6c7e207c2c04d1c
India Marsden <imm1117@ic.ac.uk>
2025-10-03 17:46:00 +0100

    cg working - draft rt test

--- test/test_orientations.py
+++ test/test_orientations.py
@@ -1,7 +1,7 @@
 import unittest.mock as mock
 from firedrake import *
 from fuse import *
-from test_convert_to_fiat import create_cg1, helmholtz_solve
+from test_convert_to_fiat import create_cg1, helmholtz_solve, construct_rt
 
 def dummy_dof_perms(cls, *args, **kwargs):
     # return -1s of right shape here
@@ -9,15 +9,43 @@ def dummy_dof_perms(cls, *args, **kwargs):
     for key1, val1 in oriented_mats_by_entity.items():
         for key2, val2 in oriented_mats_by_entity[key1].items():
             for key3, val3 in oriented_mats_by_entity[key1][key2].items():
-                oriented_mats_by_entity[key1][key2][key3] = -1 * np.identity(val3.shape[0])
+                oriented_mats_by_entity[key1][key2][key3] = 1 * np.identity(val3.shape[0])
     return oriented_mats_by_entity, False, None
 
-def test_orientation_application():
+def test_orientation_application_mocked():
     deg = 1
     with mock.patch.object(ElementTriple, 'make_dof_perms', new=dummy_dof_perms):
         cell = polygon(3)
         elem = create_cg1(cell)
-        mesh = UnitSquareMesh(4, 4)
+        mesh = UnitSquareMesh(1, 1)
 
         V = FunctionSpace(mesh, elem.to_ufl())
-        res = helmholtz_solve(V, mesh)
+        u = TestFunction(V)
+        res1 = assemble(u * dx)
+        print(res1.dat.data)
+
+        V = FunctionSpace(mesh, "CG", 1)
+        u = TestFunction(V)
+        res2 = assemble(u * dx)
+        print(res2.dat.data)
+        assert np.allclose(res1.dat.data, res2.dat.data)
+
+def test_orientation_application():
+    deg = 1
+    cell = polygon(3)
+    elem = construct_rt(cell)
+    mesh = UnitSquareMesh(1, 1)
+    ones = as_vector((1,1))
+
+    V = FunctionSpace(mesh, elem.to_ufl())
+    u = TestFunction(V)
+    x = project(ones, V)
+    res1 = assemble(dot(u, x) * dx)
+    print(res1.dat.data)
+
+    V = FunctionSpace(mesh, "RT", 1)
+    u = TestFunction(V)
+    x = project(ones, V)
+    res2 = assemble(dot(u, x) * dx)
+    print(res2.dat.data)
+    assert np.allclose(res1.dat.data, res2.dat.data)
