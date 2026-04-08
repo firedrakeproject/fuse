7518e0fdb51daa4cde302da0dfbaca8faea5cda7
India Marsden <imm1117@ic.ac.uk>
2026-04-08 17:34:44 +0100

    generation of cg1-4 on tets

--- fuse/element_construction.py
+++ fuse/element_construction.py
@@ -6,26 +6,23 @@ import sympy as sp
 from recursivenodes import recursive_nodes
 
 
-class BasisType(Enum):
-    LAGRANGE = 0
-    NEDELEC = 1
-    RAVIARTTHOMAS = 2
-
-
 def convert_to_generation(coords, verts):
     """Reduces a full list of cartesian coordinates to only those required for generation,
        and divides them into groups """
-    coords_grps = {S1: [], C3: [], diff_C3: [], S3: []}
+    coords_grps = {}
     n = len(coords)
     for c in coords:
         try:
             grp = identify_generation_group(c, verts, bary=False)
-            coords_grps[grp] += [c]
+            if grp in coords_grps.keys():
+                coords_grps[grp] += [c]
+            else:
+                coords_grps[grp] = [c]
         except ValueError:
             pass
 
-    assert n == len(coords_grps[S1]) + len(coords_grps[S3])*6 + len(coords_grps[C3])*3 + len(coords_grps[diff_C3])*3
-    return coords_grps[S1], coords_grps[C3], coords_grps[diff_C3], coords_grps[S3]
+    assert n == sum([len(coords_grps[grp])*grp.size() for grp in coords_grps.keys()])
+    return coords_grps
 
 
 def identify_generation_group(b_coord, verts, bary=True):
@@ -38,52 +35,69 @@ def identify_generation_group(b_coord, verts, bary=True):
         c = sum(b_coord[i]*np.array(verts)[i] for i in range(len(verts)))
     else:
         c = b_coord
-    verts = sorted(verts)  # ensure consistent vertex order to check points in given region
+    # ensure consistent vertex order to check points in given region
+    verts = np.array(verts)
+    inds = np.lexsort(tuple(verts[:, i] for i in range(len(verts) - 2, -1, -1))) # lex sort prioritises last row, so reverse order
+    verts = verts[inds]
+
     center = tuple(sum([v[i] for v in verts])/len(verts) for i in range(len(verts[0])))
     if np.allclose(center, c):
         return S1
 
     if len(verts) == 2:
         return S2
-
-    midpoint1 = ((verts[1][0] + verts[0][0])/2, (verts[1][1] + verts[0][1])/2)
-    midpoint2 = ((verts[0][0] + verts[2][0])/2, (verts[0][1] + verts[2][1])/2)
-    cond1 = lambda coord: check_multiple(coord, verts[0]) and check_below_line(verts[2], midpoint1, coord) <= 0
-    cond2 = lambda coord: (check_multiple(coord, midpoint2) and check_below_line(midpoint1, (0, 0), coord) <= 0)
-    # cond3 = lambda coord: (check_multiple(coord, midpoint1) and check_below_line(midpoint2, (0, 0), coord) <= 0)
-    if cond1(c):
-        return C3
-    elif cond2(c):  # cond3(c) or
-        return diff_C3
-    elif check_below_line(verts[0], (0, 0), c) == -1 and check_below_line(midpoint2, (0, 0), c) == -1:
-        return S3
+    if len(verts) == 3:
+        midpoint1 = ((verts[1][0] + verts[0][0])/2, (verts[1][1] + verts[0][1])/2)
+        midpoint2 = ((verts[0][0] + verts[2][0])/2, (verts[0][1] + verts[2][1])/2)
+        cond1 = lambda coord: check_multiple(coord, verts[0]) and check_below_line(verts[2], midpoint1, coord) >= 0
+        cond2 = lambda coord: (check_multiple(coord, midpoint2) and check_below_line(midpoint1, (0, 0), coord) >= 0)
+        if cond1(c):
+            return C3
+        elif cond2(c):  # cond3(c) or
+            return diff_C3
+        elif check_below_line(verts[0], (0, 0), c) == 1 and check_below_line(midpoint2, (0, 0), c) == 1:
+            return S3
+    if len(verts) == 4:
+        midpoint1 = (verts[1] + verts[0])/2
+        midpoint2 = (verts[0] + verts[2])/2
+        midpoint3 = (verts[1] + verts[3])/2
+        midpoint4 = (verts[2] + verts[3])/2
+        plane1 = lambda coord: check_below_plane(verts[2], verts[3], midpoint1, coord) >= 0
+        plane2 = lambda coord: check_below_plane(verts[1], verts[3], midpoint2, coord) >= 0
+        plane3 = lambda coord: check_below_plane(verts[0], verts[2], midpoint3, coord) >= 0
+        plane4 = lambda coord: check_below_plane(verts[0], verts[1], midpoint4, coord) >= 0
+        if check_multiple(c, verts[0]) and plane1(c) and plane2(c):
+            return C4
+        # add diff C4 ?? need to work out if needed
+        elif plane1(c) and plane2(c) and plane3(c) and plane4(c):
+            return tet_faces
     raise ValueError("Group not identified")
 
 
-def check_below_line(seg_1, seg_2, coord):
+def check_below_line(x_0, x_1, coord, fix_order=True):
     """
-    Checks position of coord in relation to the segment defined by seg_1 and seg_2.
+    Checks position of coord in relation to the segment defined by x_0 and x_1.
 
-    returns 0 if coord lies on the segment
-    returns -1 is coord is above the segment
-    returns 1 if coord is below the segment
+    Fixes order of input coords such that line segment is oriented left to right (or down to up),
+    returning 1 if coord is below or to the left.
     """
-    for i in range(2):
-        if np.allclose(seg_1[i] - seg_2[i], 0):
-            if np.allclose(coord[i], seg_1[i]):
-                return 0
-            elif coord[i] > seg_1[i]:
-                return 1
-            else:
-                return -1
+    if fix_order:
+        if x_1[0] < x_0[0] and not np.allclose(x_1[0], x_0[0]):
+            temp = x_0
+            x_0 = x_1
+            x_1 = temp
+        elif np.allclose(x_1[0], x_0[0]) and x_1[1] > x_0[1]:
+            temp = x_0
+            x_0 = x_1
+            x_1 = temp
+    coord = np.array(coord)
+    v_0 = np.array(x_1) - np.array(x_0)
+    n = np.matmul(np.array([[0, -1], [1, 0]]), v_0)
+    eq = lambda x: np.dot(n, x - x_0)
 
-    m = (seg_1[1] - seg_2[1])/(seg_1[0] - seg_2[0])
-    c = seg_1[1] - seg_1[0]*m
-    eq = lambda x: m*x + c
-
-    if np.allclose(eq(coord[0]), coord[1]):
+    if np.allclose(eq(coord), 0):
         return 0
-    elif eq(coord[0]) < coord[1]:
+    elif eq(coord) < 0:
         return 1
     else:
         return -1
@@ -104,7 +118,10 @@ def check_below_plane(x_0, x_1, x_2, coord):
 
 
 def check_multiple(coord_1, coord_2):
-    return np.allclose(check_below_line(coord_2, (0, 0), coord_1), 0)
+    if len(coord_1) == 2:
+        return np.allclose(check_below_line(coord_2, (0, 0), coord_1), 0)
+    div = coord_1 / coord_2
+    return np.allclose(div, div[0])
 
 
 def lagrange_barycentric_basis(dim, verts, deg):
@@ -190,33 +207,6 @@ def vector_facet_fns(cell, deg, rot=False):
     return dofs
 
 
-# def raviart_thomas_facet_fns(cell, deg):
-#     edge = cell.edges()[0]
-#     rt_basis_funcs, rt_grps, symbols = proxy_field_1_form(cell, deg, rot=True)
-#     basis_funcs, groups, _ = lagrange_barycentric_basis(edge.dimension, edge.ordered_vertex_coords(), deg - 1)
-#     dofs = []
-#     for norm, rt_grp in zip(rt_basis_funcs, rt_grps):
-#         for bf, grp in zip(basis_funcs, groups):
-#             xs = [DOF(L2Pairing(), BarycentricPolynomialKernel(norm*bf, symbols=symbols))]
-#             if grp.size() > 3:
-#                 raise NotImplementedError("Group too large")
-#             dofs += [DOFGenerator(xs, rt_grp*grp, S1)]
-
-#     basis_funcs, groups, symbols = lagrange_barycentric_basis(cell.dimension, cell.ordered_vertex_coords(), deg - 2)
-#     for bf, grp in zip(basis_funcs, groups):
-#         v_0 = np.array(cell.get_node(cell.ordered_vertices()[0], return_coords=True))
-#         v_1 = np.array(cell.get_node(cell.ordered_vertices()[1], return_coords=True))
-#         v_2 = np.array(cell.get_node(cell.ordered_vertices()[2], return_coords=True))
-#         xs = [DOF(L2Pairing(), BarycentricPolynomialKernel(np.prod(symbols)*bf*(v_0 - v_2)/2, symbols=symbols))]
-#         if grp.size() > 3:
-#             dofs += [DOFGenerator(xs, grp, S1)]
-#             xs = [DOF(L2Pairing(), BarycentricPolynomialKernel(np.prod(symbols)*bf*(v_0 - v_1)/2, symbols=symbols))]
-#             dofs += [DOFGenerator(xs, grp, S1)]
-#         else:
-#             dofs += [DOFGenerator(xs, S2*grp, S1)]
-#     return dofs
-
-
 def lagrange_facet_fns(shp, cell, deg):
     dofs = []
     basis_funcs, groups, symbols = lagrange_barycentric_basis(cell.dimension, cell.ordered_vertex_coords(), deg)
@@ -243,16 +233,13 @@ def lagrange_facet_fns(shp, cell, deg):
 def lagrange_facet_pts(cell, deg):
     from recursivenodes.nodes import _recursive, _decode_family
     from FIAT.reference_element import multiindex_equal
-    get_pt = lambda alpha: np.dot(_recursive(2, deg, alpha, _decode_family("lgl")), np.array(cell.vertices(return_coords=True)))
-    interior_coords = [tuple(pt) for pt in list(map(get_pt, multiindex_equal(3, deg, 1)))]
-    s1, c3, dc3, s3 = convert_to_generation(interior_coords, cell.ordered_vertex_coords())
+    get_pt = lambda alpha: np.dot(_recursive(cell.dimension, deg, alpha, _decode_family("lgl")), np.array(cell.vertices(return_coords=True)))
+    interior_coords = [tuple(pt) for pt in list(map(get_pt, multiindex_equal(cell.dimension + 1, deg, 1)))]
+    coords_grps = convert_to_generation(interior_coords, cell.ordered_vertex_coords())
 
-    s1 = [DOF(DeltaPairing(), PointKernel(c)) for c in s1]
-    c3 = [DOF(DeltaPairing(), PointKernel(c)) for c in c3]
-    dc3 = [DOF(DeltaPairing(), PointKernel(c)) for c in dc3]
-    s3 = [DOF(DeltaPairing(), PointKernel(c)) for c in s3]
     int_dofs = []
-    for dofs, grp in zip([s1, c3, dc3, s3], [S1, C3, diff_C3, S3]):
+    for grp in coords_grps.keys():
+        dofs = [DOF(DeltaPairing(), PointKernel(c)) for c in coords_grps[grp]]
         if len(dofs) > 0:
             int_dofs += [DOFGenerator([d], grp, S1) for d in dofs]
     return int_dofs
@@ -283,6 +270,42 @@ def construct_tri_cgN(deg):
 
     int_dofs = lagrange_facet_pts(cell, deg)
     return ElementTriple(cell, (Pk, CellL2, C0), v_dofs + edge_dofs + int_dofs)
+
+
+def construct_tet_cgN(deg):
+    cell = make_tetrahedron()
+    vert = cell.vertices()[0]
+    edge = cell.edges()[0]
+    face = cell.d_entities(2)[0]
+
+    xs = [DOF(DeltaPairing(), PointKernel(()))]
+    dg0 = ElementTriple(vert, (P0, CellL2, C0), DOFGenerator(xs, S1, S1))
+    v_xs = [immerse(cell, dg0, TrH1)]
+    v_dofs = [DOFGenerator(v_xs, C4, S1)]
+
+    points = recursive_nodes(1, deg, domain="equilateral")[1:-1].flatten()
+
+    Pk = PolynomialSpace(deg)
+    sym_points = [DOF(DeltaPairing(), PointKernel((pt,))) for pt in points[:len(points)//2]]
+    sym_dofs = [DOFGenerator([pt], S2, S1) for pt in sym_points]
+    if 0 in points:
+        centre_dof = [DOFGenerator([DOF(DeltaPairing(), PointKernel((0,)))], S1, S1)]
+    else:
+        centre_dof = []
+    edge_dg0 = ElementTriple(edge, (Pk, CellL2, C0), sym_dofs + centre_dof)
+    edge_xs = [immerse(cell, edge_dg0, TrH1)]
+    edge_dofs = [DOFGenerator(edge_xs, tet_edges, S1)]
+
+    face_dofs = lagrange_facet_pts(face, deg)
+    face_trp = ElementTriple(face, (Pk, CellL2, C0), face_dofs)
+    face_xs = [immerse(cell, face_trp, TrH1)]
+    face_dofs = [DOFGenerator(face_xs, tet_faces, S1)]
+
+    int_dofs = lagrange_facet_pts(cell, deg)
+
+    cg = ElementTriple(cell, (Pk, CellL2, C0), v_dofs + edge_dofs + face_dofs + int_dofs)
+    assert len(cg.generate()) == (deg + 1)*(deg + 2)*(deg + 3)/6
+    return cg
 
 
 def construct_tri_ndN(deg):
@@ -391,6 +414,9 @@ constructors = {
             1: construct_tri_ndN,
             2: construct_tri_rtN,
             3: construct_tri_dgNminus,
+        },
+        3: {
+            0: construct_tet_cgN,
         },
     },
     1: {
--- test/test_construction.py
+++ test/test_construction.py
@@ -2,7 +2,8 @@ import pytest
 import os
 import numpy as np
 from test_orientations import interpolate_vs_project, get_expression
-from fuse.element_construction import periodic_table, check_below_plane
+from test_convert_to_fiat import project as test_project
+from fuse.element_construction import periodic_table, check_below_plane, check_below_line
 from firedrake import *
 
 
@@ -15,6 +16,12 @@ def test_construction(k, deg):
 @pytest.mark.parametrize("k,deg", [(3, 0)] + [(k, deg) for deg in range(1, 7) for k in [0, 1, 2, 3]])
 def test_construction1(k, deg):
     elem = periodic_table(1, 2, k, deg)
+    elem.to_fiat()
+
+
+@pytest.mark.parametrize("k,deg", [(k, deg) for deg in range(1, 5) for k in [0]])
+def test_construction3d(k, deg):
+    elem = periodic_table(0, 3, k, deg)
     elem.to_fiat()
 
 
@@ -57,9 +64,72 @@ def test_convergence(col, k, deg, conv_rate):
     assert all([c > conv_rate for c in conv2])
 
 
+cg_params3d = [(0, 0, deg, deg + 0.75) for deg in list(range(1, 5))]
+
+
+@pytest.mark.parametrize("col,k,deg,conv_rate", cg_params3d)
+def test_convergence3d(col, k, deg, conv_rate):
+    assert bool(os.environ.get("FIREDRAKE_USE_FUSE", 0))
+    elem = periodic_table(col, 3, k, deg)
+    # from test_convert_to_fiat import create_cg1_tet
+    # from fuse import make_tetrahedron
+    # elem = create_cg1_tet(make_tetrahedron())
+    scale_range = range(2, 5)
+    diff_proj = [0 for i in scale_range]
+    for n in scale_range:
+        mesh = UnitCubeMesh(2**n, 2**n, 2**n)
+
+        V = FunctionSpace(mesh, elem.to_ufl())
+        x, y, z = SpatialCoordinate(mesh)
+        expr = cos(x*pi*2)*sin(y*pi*2)
+        if len(elem.get_value_shape()) > 0:
+            expr = as_vector([expr, expr, expr])
+        diff_proj[n-min(scale_range)] = test_project(V, mesh, expr)
+    
+    print("projection l2 error norms:", diff_proj)
+    diff_proj = np.array(diff_proj)
+    conv1 = np.log2(diff_proj[:-1] / diff_proj[1:])
+    print("convergence order:", conv1)
+    assert all([c > conv_rate for c in conv1])
+
+
+def test_proj3d_firedrake():
+    conv_rate = 1.75
+    scale_range = range(2, 5)
+    diff_proj = [0 for i in scale_range]
+    for n in scale_range:
+        mesh = UnitCubeMesh(2**n, 2**n, 2**n)
+
+        V = FunctionSpace(mesh, "CG", 1)
+        x, y, z = SpatialCoordinate(mesh)
+        expr = cos(x*pi*2)*sin(y*pi*2)
+        diff_proj[n-min(scale_range)] = test_project(V, mesh, expr)
+
+    print("projection l2 error norms:", diff_proj)
+    diff_proj = np.array(diff_proj)
+    conv1 = np.log2(diff_proj[:-1] / diff_proj[1:])
+    print("convergence order:", conv1)
+    assert all([c > conv_rate for c in conv1])
+
+
+
 def test_plane():
     from fuse import make_tetrahedron
     cell = make_tetrahedron()
     verts = cell.ordered_vertex_coords()
     res = check_below_plane(verts[1], verts[2], verts[3], (verts[1] + verts[2] + verts[3])/3)
     print(res)
+
+
+def test_check_line():
+    from fuse import polygon
+    cell = polygon(3)
+    verts = np.array(sorted(cell.ordered_vertex_coords()))
+    midpoint = (verts[1] + verts[2])/2
+    midpoint1 = (verts[0] + verts[2])/2
+    assert check_below_line(verts[0], midpoint, (0, 0)) == 0
+
+    assert check_below_line(verts[0], midpoint, (-0.5, 0)) == -1
+    assert check_below_line(verts[0], midpoint, (0, -0.5)) == 1
+
+    assert check_below_line(verts[1], midpoint1, verts[0]) == 1
