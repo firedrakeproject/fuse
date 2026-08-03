746f8b8ec1a2a46acc378eafb9edee8cf8c84203
India Marsden <imm1117@ic.ac.uk>
2026-08-03 09:44:37 +0100

    git branches and make firedrake optional dep

--- .github/workflows/setup_repos.sh
+++ .github/workflows/setup_repos.sh
@@ -6,7 +6,7 @@
 /usr/bin/git config --global --add safe.directory /opt/firedrake/
 cd /opt/firedrake/
 git fetch
-git checkout indiamai/fuse-quads
+git checkout indiamai/fuse
 git pull
 pip install pybind11 pyrsistent Cython
 make
@@ -17,7 +17,7 @@ git clone https://github.com/firedrakeproject/fiat.git
 /usr/bin/git config --global --add safe.directory ~/fiat
 cd fiat
 git fetch
-git checkout indiamai/integrate_fuse
+git checkout indiamai/fuse
 git status
 python3 -m pip install --break-system-packages -e .
 
