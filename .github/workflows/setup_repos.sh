0ddd137137bd7426efda4815495f940b376fbfd0
India Marsden <imm1117@ic.ac.uk>
2026-08-04 10:40:17 +0100

    remove outdated setup

--- .github/workflows/setup_repos.sh
+++ .github/workflows/setup_repos.sh
@@ -1,8 +1,3 @@
-# NOTE: the branches below differ from those the test suite runs against in
-# test.yml (firedrake indiamai/fuse_mesh_cell, fiat indiamai/fuse). The docs
-# are therefore built against a different dependency set from the one that is
-# tested. Unify these once it is confirmed the docs build on the tested pair.
-
 /usr/bin/git config --global --add safe.directory /opt/firedrake/
 cd /opt/firedrake/
 git fetch
@@ -20,13 +15,3 @@ git fetch
 git checkout indiamai/fuse
 git status
 python3 -m pip install --break-system-packages -e .
-
-#/usr/bin/git config --global --add safe.directory ~
-#cd ~
-#git clone https://github.com/firedrakeproject/ufl.git
-#/usr/bin/git config --global --add safe.directory ~/ufl
-#cd ufl
-#git fetch
-#git checkout indiamai/integrate-fuse
-#git status
-#python3 -m pip install --break-system-packages -e .
